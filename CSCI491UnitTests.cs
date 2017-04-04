c03b5775b25fd0c668f7dfd63f4c1693aa32a089
Jake Petek <jakedpetek@gmail.com>
2017-04-04 04:53:27 -0600

    Update CSCI491UnitTests.cs
    Changed unit tests so that they work with the data managers.
    Properly formatted test cases for Visual Studio Project.

--- CSCI491UnitTests.cs
+++ CSCI491UnitTests.cs
@@ -1,102 +1,113 @@
 //CSCI491 unit tests
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MySql.Data.MySqlClient;
+using System.Collections.Generic;
 
-@Before
-//set up a conncetion and a command for MySql
-MySql.Data.MySqlClient.MySqlCommand com = new MySql.Data.MySqlClient.MySqlCommand();
-
-//create an empty database for testing that is an exact copy but empty
-mysqldump -u username –p  -d database_name|mysql -u username -p new_database
-
-//establish connection with test database
-conn = new MySql.Data.MySqlClient.MySqlConnection("CONNECTION");
-com.Connection = conn;
-
-
-//tests for add
-
-@Test 
-public void testAddOrg()
+[TestClass]
+public class UnitTest1
 {
-	add(123456, "tester");
-	com.CommandText = "SELECT npi FROM organization WHERE npi == 123456";
-	int result = int.Parse(com.ExecuteScalar().ToString());
-	assertEqual(result, 123456, "testing add organizer to an empty db");
-}
 
-@Test
-public void testAddDupOrg()
-{
-	add(123456, "tester");
-	add(123456, "tester");
-	com.CommandText = "SELECT COUNT(*) FROM organization";
-	int result = int.Parse(com.ExecuteScalar().ToString());
-	assertEqual(result,1, "testing applying duplicate organization");
-}
+    //set up a conncetion and a command for MySql
+    MySqlCommand com = new MySqlCommand();
 
+    //create an empty database for testing that is an exact copy but empty
+    //"mysqldump -u username –p  -d database_name|mysql -u username -p new_database";
 
-@Test
-public void testAddProv()
-{
-	add(123456);
-	com.CommandText = "SELECT npi FROM provider WHERE npi == 123456";
-	int result = int.Parse(com.ExecuteScalar().ToString());
-	assertEqual(result, 123456, "testing adding provider to an empty db");
-}
+    //establish connection with test database
+    MySqlConnection conn = new MySqlConnection("CONNECTION");
+    //com.Connection = conn;
+    OrganizationManager orgManager = new OrganizationManager("npi_organization_data");
+    ProviderManager proManager = new ProviderManager("npi_provider_data");
+    DeactivationManager deaManager = new DeactivationManager("npi_deactivated");
 
-@Test
-public void testAddDupProv()
-{
-	add(123456);
-	add(123456);
-	com.CommandText = "SELECT COUNT(*) FROM provider";
-	int result = int.Parse(com.ExecuteScalar().ToString());
-	assertEqual(result,1, "testing applying duplicate provider");
-}
+    Entry testEntryOrg = new Entry(new List<string>(new string[]{ "123456", "tester" }));
+    Entry testEntryPro = new Entry(new List<string>(new string[] { "123456" }));
+    //tests for add
 
-//tests for find
+    [TestMethod]
+    public void testAddOrg()
+    {
+        orgManager.AddEntity(testEntryOrg);
+        com.CommandText = "SELECT npi FROM organization WHERE npi == 123456";
+        int result = int.Parse(com.ExecuteScalar().ToString());
+        Assert.AreEqual(result, 123456, "testing add organizer to an empty db");
+    }
 
-@Test
-public void testFindOrg()
-{
-	add(123456,"Tester");
-	Organization testOrg = new Organization();
-	testOrg = find(123456, "Tester");
-	assertEqual(testOrg.npi, 123456, "testing finding an organization on an empty db");
-}
-
-@Test
-public void testFindProv()
-{
-	add(123456);
-	Provider testProv = new Provider();
-	testProv = find(123456);
-	assertEqual(testProv.npi, 123456, "testing finding a Provider on an empty db");
-}
+    [TestMethod]
+    public void testAddDupOrg()
+    {
+        orgManager.AddEntity(testEntryOrg);
+        orgManager.AddEntity(testEntryOrg);
+        com.CommandText = "SELECT COUNT(*) FROM organization";
+        int result = int.Parse(com.ExecuteScalar().ToString());
+        Assert.AreEqual(result, 1, "testing applying duplicate organization");
+    }
 
 
-//tests for update
+    [TestMethod]
+    public void testAddProv()
+    {
+        proManager.AddEntity(testEntryPro);
+        com.CommandText = "SELECT npi FROM provider WHERE npi == 123456";
+        int result = int.Parse(com.ExecuteScalar().ToString());
+        Assert.AreEqual(result, 123456, "testing adding provider to an empty db");
+    }
 
-@Test
-public void testUpdate()
-{
-	update();
-	Provider testProv = new Provider();
-	testProv = find(123456);
-	assertEqual(testProv.npi, 123456, "testing applying an update file on an empty db");
-}
+    [TestMethod]
+    public void testAddDupProv()
+    {
+        proManager.AddEntity(testEntryPro);
+        proManager.AddEntity(testEntryPro);
+        com.CommandText = "SELECT COUNT(*) FROM provider";
+        int result = int.Parse(com.ExecuteScalar().ToString());
+        Assert.AreEqual(result, 1, "testing applying duplicate provider");
+    }
+
+    //tests for find
+
+    [TestMethod]
+    public void testFindOrg()
+    {
+        orgManager.AddEntity(testEntryOrg);
+        Organization testOrg = new Organization();
+        testOrg = find(123456, "Tester");
+        Assert.AreEqual(testOrg.npi, 123456, "testing finding an organization on an empty db");
+    }
+
+    [TestMethod]
+    public void testFindProv()
+    {
+        proManager.AddEntity(testEntryPro);
+        Provider testProv = new Provider();
+        testProv = find(123456);
+        Assert.AreEqual(testProv.npi, 123456, "testing finding a Provider on an empty db");
+    }
 
 
-//tests for deactivate
+    //tests for update
 
-@Test 
-public void testUpdate()
-{
-	update();
-	//use the same file that was used to update, should cause the table to be empty
-	deactivate();
-	com.CommandText = "SELECT COUNT(*) FROM organization";
-	int result = int.Parse(com.ExecuteScalar().ToString());
-	assertEqual(result,0, "testing applying deactivate that should remove the only row causing it to be empty");
+    [TestMethod]
+    public void testUpdate()
+    {
+        update();
+        Provider testProv = new Provider();
+        testProv = find(123456);
+        Assert.AreEqual(testProv.npi, 123456, "testing applying an update file on an empty db");
+    }
+
+
+    //tests for deactivate
+
+    [TestMethod]
+    public void testDeactivate()
+    {
+        update();
+        //use the same file that was used to update, should cause the table to be empty
+        deactivate();
+        com.CommandText = "SELECT COUNT(*) FROM organization";
+        int result = int.Parse(com.ExecuteScalar().ToString());
+        Assert.AreEqual(result, 0, "testing applying deactivate that should remove the only row causing it to be empty");
+    }
 }
 
 
